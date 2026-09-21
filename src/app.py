ef7bc0d98b0d2ee73878cadf71e195d15d0cb976
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-21 11:18:42 -0600

    Replace ZeroTrustStack with IdentityScopedDataAccessStack

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+
 import os
+
 import aws_cdk as cdk
-from zero_trust_stack import ZeroTrustStack
+
+from zero_trust_stack import IdentityScopedDataAccessStack
+
 
 app = cdk.App()
 
@@ -10,6 +14,10 @@ env = cdk.Environment(
     region=os.getenv("CDK_DEFAULT_REGION") or "us-east-1",
 )
 
-ZeroTrustStack(app, "Sec01ZeroTrustStack", env=env)
+IdentityScopedDataAccessStack(
+    app,
+    "IdentityScopedDataAccessStack",
+    env=env,
+)
 
 app.synth()
